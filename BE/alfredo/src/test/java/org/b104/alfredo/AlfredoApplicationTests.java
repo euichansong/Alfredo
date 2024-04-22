60f824e1f5152b4bbd4339aca8e35413d3ecc0eb
chosj33 <chosj33@naver.com>
2024-04-22 13:38:46 +0900

    be fix

---
+++ BE/alfredo/src/test/java/org/b104/alfredo/AlfredoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.b104.alfredo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfredoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
