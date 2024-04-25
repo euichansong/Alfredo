99a1e1af529cf9d2906b6fba5d7069f836e92f31
Sooji Kwon <gkdrhd6788@gmail.com>
2024-04-25 16:21:40 +0900

    Feat: 루틴 추가

--- BE/alfredo/src/main/java/org/b104/alfredo/AlfredoApplication.java
+++ BE/alfredo/src/main/java/org/b104/alfredo/AlfredoApplication.java
@@ -2,9 +2,8 @@ package org.b104.alfredo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class AlfredoApplication {
 
     public static void main(String[] args) {
