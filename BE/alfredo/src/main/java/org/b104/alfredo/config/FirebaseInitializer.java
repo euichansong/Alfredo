1e02f6210130ff3f5faa8101bf03d6986dbde85b
Sooji Kwon <gkdrhd6788@gmail.com>
2024-04-26 13:46:09 +0900

    ADD:properties 수정

--- BE/alfredo/src/main/java/org/b104/alfredo/config/FirebaseInitializer.java
+++ BE/alfredo/src/main/java/org/b104/alfredo/config/FirebaseInitializer.java
@@ -3,6 +3,7 @@ package org.b104.alfredo.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -12,12 +13,15 @@ import java.io.IOException;
 @Component
 public class FirebaseInitializer {
 
+    @Value("${firebase.sdk}")
+    private String sdkKey;
+
     @PostConstruct
     public void initialize() {
         try {
             // 환경변수에서 Firebase 설정 파일의 경로를 읽어옵니다.
             FileInputStream serviceAccount =
-                    new FileInputStream("C:\\Users\\SSAFY\\Desktop\\class\\S10P31B104_BE\\BE\\alfredo\\alfredo104-firebase-adminsdk-qhdta-be4ef2b790.json");
+                    new FileInputStream(sdkKey);
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
