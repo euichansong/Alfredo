b6eb688001adf7d8f68e9348ad87b6fd0930577b
권근열 <yeol3011@naver.com>
2024-04-29 09:19:53 +0900

    [feat] 버튼

--- FE/alfredo/lib/screens/mainpage/main_page.dart
+++ FE/alfredo/lib/screens/mainpage/main_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import "package:persistent_bottom_nav_bar/persistent_tab_view.dart";
+import 'package:alfredo/components/navbar.dart';
 
 // 여기다가 각자 작업하는 페이지로 가는 push 버튼 만들어서 작업 ㄱㄱ
 class MainPage extends StatelessWidget {
@@ -19,12 +21,13 @@ class MainPage extends StatelessWidget {
                 print("Logged out"); // 정상적으로 로그아웃
                 Navigator.pushReplacementNamed(context, '/'); // 로그인 페이지로 리디렉션
               } catch (e) {
-                print("Logout failed: $e");
+                print("Logout failed: $e"); // 로그아웃 실패
               }
             },
           ),
         ],
       ),
+      // body: CustomNavBar(),
     );
   }
 }
