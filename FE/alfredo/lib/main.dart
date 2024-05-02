45a68949cb000612c291f4acf772cc18db2ca38f
Sooji Kwon <gkdrhd6788@gmail.com>
2024-05-02 14:43:54 +0900

    환경 설정

--- FE/alfredo/lib/main.dart
+++ FE/alfredo/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'config/firebase_options.dart';
 import 'screens/mainpage/main_page.dart'; // 여기에 MainPage 파일을 import 합니다.
@@ -8,11 +10,12 @@ import 'screens/user/user_routine_test.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
