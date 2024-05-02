eefb7eabaad1079f470117cc4b012f6a5a91f98a
euichansong <zxcasdqwer38@naver.com>
2024-05-02 09:39:22 +0900

    Fix: api 경로 매핑

--- FE/alfredo/lib/api/schedule/schedule_api.dart
+++ FE/alfredo/lib/api/schedule/schedule_api.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import '../../models/schedule/schedule_model.dart';
 
 class ScheduleApi {
-  final String baseUrl = 'http://10.0.2.2:8080';
+  final String baseUrl = 'http://10.0.2.2:8080/api/schedule';
 
   ScheduleApi();
 
