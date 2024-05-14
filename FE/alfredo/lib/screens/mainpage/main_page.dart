6a631a819fcee35af4f31a7e5714372d8ca3ad77
rlaxodhks770 <rlaxodhks770@naver.com>
2024-05-14 15:32:30 +0900

    feat : 아이콘 해결

--- FE/alfredo/lib/screens/mainpage/main_page.dart
+++ FE/alfredo/lib/screens/mainpage/main_page.dart
@@ -26,8 +26,36 @@ class _MainPageState extends ConsumerState<MainPage> {
           Container(
             decoration: const BoxDecoration(
               image: DecorationImage(
-                image: AssetImage('assets/mainalfredo.png'),
+                image: AssetImage('assets/mainback1.png'),
                 fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Positioned.fill(
+            top: MediaQuery.of(context).size.height * 0.3,
+            left: MediaQuery.of(context).size.height * 0,
+            right: MediaQuery.of(context).size.height * 0,
+            bottom: MediaQuery.of(context).size.height * 0.1,
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/image 12.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Positioned.fill(
+            top: MediaQuery.of(context).size.height * 0.4,
+            left: MediaQuery.of(context).size.height * 0,
+            right: MediaQuery.of(context).size.height * 0,
+            bottom: MediaQuery.of(context).size.height * 0.05,
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/image 11.png'),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
           ),
